0087635dc1190c473fc3abb9b8bd4c2e55ce2e99
Masanori Ikda <bma4z7k7v@yahoo.co.jp>
2019-07-07 12:41:20 +0900

    export data files, bug fix, refactor

--- japan.js
+++ japan.js
@@ -1,66 +1,65 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+createFiles();
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://prichan.jp/shop/tokyo.html');
-
   await page.goto('https://prichan.jp/shop/');
-  var areas = await page.$$(".shopLink");
 
-  await console.log("");  
-  await console.log("-------------name---------------");  
-  await console.log("");  
+  var classAreas = await page.$$(".shopLink");
+  for(var areaCount = await 0; areaCount < await classAreas.length; await areaCount++) {
+    var liPrefectures = await getLiPrefectures(classAreas[areaCount]);
 
-  for(var url =0; url<7; url++) {
-    var areas2 = await page.$$(".shopLink");
-    var dd = await areas2[url].$('dd');
-
-    var lis = await dd.$$('li');
-    for(var url2 =0; url2<lis.length; url2++) {
-      const a = await lis[url2].$('a');
-      const link = await a.getProperty('href');
-      await page.goto(await link.jsonValue("_remoteObject"));
+    for(var prefectureCount = await 0; prefectureCount < await liPrefectures.length; await prefectureCount++) {
+      await page.goto(await getLinkToPrefecture(await liPrefectures[prefectureCount]));
     
-      const names = await page.$$(".name");
-      for(const name of names) {
-        const elem = await name.getProperty('innerText');
-        await console.log(await elem.jsonValue("_remoteObject"));  
-      }  
+      await writePrefecturesData("name", await page.$$(".name"));
+      await writePrefecturesData("add", await page.$$(".add"));
+
       await page.goto('https://prichan.jp/shop/');
-      areas2 = await page.$$(".shopLink");
-      dd = await areas2[url].$('dd');
-  
-      lis = await dd.$$('li');
-      }
-  }
-
-  await console.log("");  
-  await console.log("-------------add---------------");  
-  await console.log("");  
-
-  for(var url =0; url<7; url++) {
-    var areas2 = await page.$$(".shopLink");
-    var dd = await areas2[url].$('dd');
-
-    var lis = await dd.$$('li');
-    for(var url2 =0; url2<lis.length; url2++) {
-      const a = await lis[url2].$('a');
-      const link = await a.getProperty('href');
-      await page.goto(await link.jsonValue("_remoteObject"));
-    
-      const names = await page.$$(".add");
-      for(const name of names) {
-        const elem = await name.getProperty('innerText');
-        await console.log(await elem.jsonValue("_remoteObject"));  
-      }  
-      await page.goto('https://prichan.jp/shop/');
-      areas2 = await page.$$(".shopLink");
-      dd = await areas2[url].$('dd');
-  
-      lis = await dd.$$('li');
-      }
+      classAreas = await page.$$(".shopLink");
+      liPrefectures = await getLiPrefectures(classAreas[areaCount]);
+    }
   }
 
   await browser.close();
 })();
+
+function createFiles() {
+  createFile("name.txt");
+  createFile("add.txt");
+}
+
+async function getLiPrefectures(classArea) {
+  const ddArea = await classArea.$('dd');
+  return await ddArea.$$('li');
+}
+
+async function getLinkToPrefecture(prefecture) {
+  const a = await prefecture.$('a');
+  const link = await a.getProperty('href');
+  return await link.jsonValue("_remoteObject");
+}
+
+async function writePrefecturesData(filename, dataClasses) {
+  for(const data of dataClasses) {
+    const elem =  await data.getProperty('innerText');
+    const text = await elem.jsonValue("_remoteObject");  
+    await appendFile(await filename + ".txt", await text + "\n");  
+  }  
+}
+
+function createFile(path) {
+  fs.writeFileSync(path, "", function (err) {
+    console.log(err);
+  });
+}
+
+function appendFile(path, data) {
+  fs.appendFileSync(path, data, function (err) {
+    if (err) {
+        throw err;
+    }
+  });
+}
--- tokyo.js
+++ tokyo.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+var fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -9,20 +10,41 @@ const puppeteer = require('puppeteer');
   await console.log("-------------name---------------");  
   await console.log("");  
   const names = await page.$$(".name");
+  await createFile("name.txt");
+
   for(const name of names) {
     const elem = await name.getProperty('innerText');
-    await console.log(await elem.jsonValue("_remoteObject"));  
+    await console.log(await elem.jsonValue("_remoteObject"));
+    const text = await elem.jsonValue("_remoteObject");
+    await appendFile("name.txt", await text + "\n");
   }
 
   await console.log("");  
   await console.log("-------------add---------------");  
   await console.log("");  
   const adds = await page.$$(".add");
+  await createFile("add.txt");
+
   for(const add of adds) {
     const elem = await add.getProperty('innerText');
     await console.log(await elem.jsonValue("_remoteObject"));  
+    const text = await elem.jsonValue("_remoteObject");
+    await appendFile("add.txt", await text + "\n");
   }
-//  await page.screenshot({path: 'screenshot.png'});
 
   await browser.close();
 })();
+
+function createFile(path) {
+  fs.writeFileSync(path, "", function (err) {
+    console.log(err);
+  });
+}
+
+function appendFile(path, data) {
+  fs.appendFileSync(path, data, function (err) {
+    if (err) {
+        throw err;
+    }
+  });
+}
